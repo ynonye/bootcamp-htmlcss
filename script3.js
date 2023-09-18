bd1e4ce0dcf43b859e55ac5f7873cba5c1ce2246
pelech-tech <pelech.tech@gmail.com>
2023-09-18 14:26:39 +0300

    pushing ex 4.1

---
+++ script3.js
@@ -0,0 +1,13 @@
+let numSiblings = 0; 
+
+console.log('How many siblings do you have?');
+
+if (numSiblings === 1) {
+    console.log('1 sibling');
+} else if (numSiblings > 1 ) {
+    console.log('More then 1 sibling');
+}else {
+    console.log('No sibling');
+}
+
+ 
