ed81d8a5ddff99649a8ddb33e80c7247c0ea8734
pelech-tech <pelech.tech@gmail.com>
2023-09-18 15:39:37 +0300

    pushing ex. 4.2

---
+++ scrip4.js
@@ -0,0 +1,13 @@
+let grade = console.log('What is your grade (please enter it in a form of a letter)');
+let upperGrade = grade.toUpperCase();
+
+if (grade > 65 ) {
+    console.log('you have recevied F')
+}else if ( 65 && 69) {
+    console.log('you have reecived a D')
+}else if (70 && 79) {
+    console.log('you have recevied an C')
+}else if (80 && 89) {
+    console.log('you have recevied a B')
+}else {'you have recevied an A'}  
+
