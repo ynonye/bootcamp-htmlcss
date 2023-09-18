2113ad01a5e490984df6972a8830c7f1bb40f462
pelech-tech <pelech.tech@gmail.com>
2023-09-18 16:10:28 +0300

    loading basketball ex

---
+++ script5.js
@@ -0,0 +1,15 @@
+let johnteam = 89 + 120 + 103;
+let mikeTeam = 116 + 94 + 123; 
+let maryTeam = 97 + 134 + 105;
+
+johnAvrage = jhonTeam / 3;
+mikeAvrege = mikeTeam / 3;
+maryAvrege = maryTeam / 3;
+
+console.log('The winner is mike team which scored mikeAvrege');
+
+let johnTeam = 117 + 95 + 133; 
+
+console.log('The winner is John team which scored')
+console.log("mary's have scored an avareage of 112" )
+
