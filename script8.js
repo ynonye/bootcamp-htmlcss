f52273a70b59d39b36f85498565c7047cdd6f274
pelech-tech <pelech.tech@gmail.com>
2023-09-19 10:58:58 +0300

    adding the last two ex

---
+++ script8.js
@@ -0,0 +1,20 @@
+const people = ["Greg", "Mary", "Devon", "james"];
+
+people.shift[0];
+
+people.pop[-1];
+
+people.unshift["Matt"];
+
+people.push["Ynon"];
+
+people.slice[1,4];
+
+people.indexOf[1];
+
+people.indexOf["Foo"];
+
+const people1 = people;
+
+people1.splice
+
---
+++ script9.js
@@ -0,0 +1,5 @@
+const array = [1, 22, 55, "Ynon", "Netha"];
+
+let newArry = array.length;
+
+console.log(newArry);
