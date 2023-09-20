a09f1408a0d2a317b2791205b7a0e7bcfd55b152
pelech-tech <pelech.tech@gmail.com>
2023-09-20 15:34:37 +0300

    uploading some chenges

---
+++ script24.js
@@ -0,0 +1,9 @@
+const book = {
+    bookName : 'don\'t touch me',
+    author : 'Jennifer conly',
+    pages : '442' ,
+    genre : 'Fantasy',
+    year : '2021'
+};
+
+console.log(`The book ${book.bookName} was written by ${book.author} in the year ${book.year}.`)
