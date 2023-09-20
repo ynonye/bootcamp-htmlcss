e93027cc2b1593d5cff41d13f4b76be078f7a0a4
pelech-tech <pelech.tech@gmail.com>
2023-09-20 12:48:31 +0300

    pushing 20-22

---
+++ script20.js
@@ -0,0 +1,10 @@
+const listOfNeighbors = [
+    ["Canada", "Mexico"],
+    ["Spain"],
+    ["Norway", "Sweden", "Russia"],
+  ];
+  for (let i = 0; i < listOfNeighbors.length; i++) {
+      for(let j = 0;j<listOfNeighbors[i].length;j++){
+          console.log(`Neighbor: ${listOfNeighbors[i][j]}`);
+      }
+  }
---
+++ script21.js
@@ -0,0 +1,12 @@
+const randomNumber = Math.floor(Math.random() * 51); // Generates a random integer between 0 and 50 (inclusive)
+console.log(randomNumber);
+let guess = prompt("guess a number between 0 and 50");
+while (guess != randomNumber) {
+  if (guess < randomNumber) {
+    console.log(`too low`);
+  } else {
+    console.log(`too high`);
+  }
+  guess = prompt("Try Again! guess a number between 0 and 50");
+}
+console.log(`You've Guessed Right! The Number is ${randomNumber}`);
