3255a16d4ff4815f8d05aef55821297dd9e7cbbb
pelech-tech <pelech.tech@gmail.com>
2023-09-20 11:42:11 +0300

    pushing the last ex.

--- script17.js
+++ script17.js
@@ -2,6 +2,13 @@ const myCountry = {
     country: "Lapland",
     capital: "lilput",
     language: "Hebrew",
-    neighbours: "norway"
+    neighbours: "norway",
+    describe: function () {
+        return this.("finland has a 6 milions people their mother tongue is Finnish, they have 3 neighbouring countries and a capital called Heksinki");  ; 
+    } 
 }
+
+
+
+
 
