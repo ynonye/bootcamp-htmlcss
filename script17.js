b70ebff65d5791e692bf006bb6f72591e302b653
pelech-tech <pelech.tech@gmail.com>
2023-09-20 14:50:41 +0300

    updating ex 17

--- script17.js
+++ script17.js
@@ -1,10 +1,11 @@
 const myCountry = {
     country: "Lapland",
     capital: "lilput",
+    population: 7000000,
     language: "Hebrew",
     neighbours: "norway",
     describe: function () {
-        console.log(`${this.country} has ${this.population} people, their mother tongue is ${this.language} they have ${this.neighbors.length} neighboring countries and a capital called ${this.capital}.`); 
+        console.log(`${this.country} has ${this.population} people, their mother tongue is ${this.language} and a capital called ${this.capital}.`); 
     }, 
 
     isIsland:function(){
@@ -12,7 +13,7 @@ const myCountry = {
     }
   };
   myCountry.describe()
-  console.log(mycountry.isIsland());
+  console.log(myCountry.isIsland());
 
 
 
---
+++ script22.js
@@ -0,0 +1,7 @@
+const array = ["Hello", "Good day", "Your Welcome", "hotdog", "hamburgers"];
+
+function conutingLetters(arry) {
+  for(let char of'abcdefghijklmnopqrstuvwxyz') {
+    console.log(char);
+  }
+}
