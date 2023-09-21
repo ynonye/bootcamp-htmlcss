b705f920fa06583260f68df91a72e628b7f68e42
pelech-tech <pelech.tech@gmail.com>
2023-09-21 10:33:48 +0300

    finished ex.2

---
+++ javascript/functions/functions2.js
@@ -0,0 +1,12 @@
+const jobTitle = 'progammer';
+const loaction = 'New York';
+const partnerName = 'Rana';
+const nuberOfChildren = 2; 
+
+
+
+function tellFortune(jobTitle, location, partnerName, nuberOfChildren) {
+    return (`You will be a ${jobTitle} in ${loaction} married to ${partnerName} with ${nuberOfChildren}`);
+}
+
+console.log((tellFortune));
