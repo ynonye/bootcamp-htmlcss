c1bf88b74595f9f937e4a5603923ef26fae0531c
pelech-tech <pelech.tech@gmail.com>
2023-09-21 10:13:11 +0300

    loading th 1 ex

---
+++ javascript/functions/functions1.js
@@ -0,0 +1,9 @@
+const country = 'spain';
+const population = '47 million';
+const capitalCity = 'Madrid';
+
+function countryInfo(country, population, capitalCity) {
+    return (`${country} has ${population} and its capital city is ${capitalCity}`);
+}
+
+console.log(countryInfo(country, population, capitalCity));
