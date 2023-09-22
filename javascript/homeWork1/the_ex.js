acd2b877d76f78400606262046a5b31dbbf2abd4
pelech-tech <pelech.tech@gmail.com>
2023-09-22 08:22:03 +0300

    compleated ex.2

--- javascript/homeWork1/the_ex.js
+++ javascript/homeWork1/the_ex.js
@@ -1,12 +1,26 @@
 //ex 1.1
 
-function yesNo(bool) {
-    if (bool === true)
-        return 'yes'; 
-    } else if (bool === false) {
-        return 'no';
-    }
-    return `invalis argument`;
-}
+//function yesNo(bool) {
+//    if (bool === true)
+//        return 'yes'; 
+//    } else {
+//        return 'no';    
+//    }
 
-console.log(yesNo());
+//    console.log(yesNo());
+
+//ex 2.1
+
+//function sumOfTheLowest(a, b) {
+//    return a-b; 
+//}
+//function sumTheLowest(array) {
+//    array.sort(sumOfTheLowest);
+//    return array[0] + [1];
+//}
+//const array = [10 , 101, 4, 3 , 8, 1, 0, 450]
+//console.log(sumOfTheLowest(array));
+    
+// ex 2.2
+
+
