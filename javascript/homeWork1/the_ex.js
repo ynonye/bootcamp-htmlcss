85547ce82d31d17540f9b4fee35cb8f363496b2b
pelech-tech <pelech.tech@gmail.com>
2023-09-22 08:52:41 +0300

    finished the theerd ex

--- javascript/homeWork1/the_ex.js
+++ javascript/homeWork1/the_ex.js
@@ -24,3 +24,22 @@
 // ex 2.2
 
 
+//function toDecimal(v) {
+//    let binary = '';
+//    if(typeof v == 'string') {
+//      binary = v.split();
+//    } else {
+//        binary = v.toString().split();
+//    }
+//    let decimal = 0;
+//    for(let i = 0; i < binary.length; i++) {
+//        decimal = (decimal * 2) + binary[i];
+//    }
+//    return decimal;
+//  }
+
+//ex 2.3
+
+
+
+  
