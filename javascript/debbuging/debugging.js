4fa18a18f8b016852f9b63465e1981f20357df09
pelech-tech <pelech.tech@gmail.com>
2023-09-26 16:02:57 +0300

    Pushingthe last ex.

--- javascript/debbuging/debugging.js
+++ javascript/debbuging/debugging.js
@@ -4,6 +4,5 @@ function getSumOfEven(arr){
     getSumOfEven([1,2,3,4,5,6,7,8,9,10]);
 
 // The problematic line is: is line 2 were not all the elements are at the same line.
-//   
-//       
-
+// JS linter.    
+// indexes in an array start from 0 so to get even cells we need to start from arr[1] and add two every time to the index to stay in even cell this also causes a problem when calling arr[10].
