c9c0b1b06e88028a72202b212fe172ba7c3ee1ac
pelech-tech <pelech.tech@gmail.com>
2023-09-26 15:32:45 +0300

    pushing ex. 15.3

--- javascript/debbuging/debugging.js
+++ javascript/debbuging/debugging.js
@@ -1,8 +1,9 @@
 function getSumOfEven(arr){
-    return arr[2] + arr[4] + arr[6] + arr[8] +
-    arr[10];
+    return arr[2] + arr[4] + arr[6] + arr[8] +  arr[10];
     }
     getSumOfEven([1,2,3,4,5,6,7,8,9,10]);
 
-// The problematic line is:      
+// The problematic line is: is line 2 were not all the elements are at the same line.
+//
+//       
 
