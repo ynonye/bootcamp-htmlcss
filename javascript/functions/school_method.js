d7ed6be6499d8fd04c6a7c8e825556e1138a18ff
pelech-tech <pelech.tech@gmail.com>
2023-09-26 12:27:45 +0300

    uploading all rellvant files

---
+++ javascript/functions/school_method.js
@@ -0,0 +1,42 @@
+const school = {
+    teachers: [
+    {
+    id: 1,
+    name: "Pinchas",
+    subjects: ["chemistry", "biology", "physics"],
+    students: [],
+    capacityLeft: 3,
+    },
+    {
+    id: 2,
+    name: "Williams",
+    subjects: ["history", "ethics"],
+    students: [],
+    capacityLeft: 2,
+    },
+    ],
+    students: [
+    {
+    id: 10,
+    name: "Jennifer",
+    age: 20,
+    },
+    {
+    id: 11,
+    name: "Howard",
+    age: 23,
+    },
+    {
+    id: 12,
+    name: "Old-Timmy",
+    age: 86,
+    },
+    {
+    id:13,
+    name: "Houston",
+    age: 21,
+    },
+    ],
+    }
+
+    
