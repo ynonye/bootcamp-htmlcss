c7e00dd6ed7fe0f07efe9c0db6e9e76e77a541f6
pelech-tech <pelech.tech@gmail.com>
2023-09-26 14:28:00 +0300

    pushing questions no 15.4

---
+++ javascript/debbuging/debugging2.js
@@ -0,0 +1,11 @@
+function calcAverage (arr){
+    let sum ;
+    for ( let i = 0 ; i < arr .length; i ++ ){
+    sum += arr [ i ];
+    } return sum ;
+    }
+    calcAverage ([ 85 , 90 , 92 ]);
+
+// we don't use the "var" any more.
+// I've seen it with my own eyes.
+// I fixed the bug.      
