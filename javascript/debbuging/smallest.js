dbc3e786d0c38f6d13a81278575f4f154a8511fd
pelech-tech <pelech.tech@gmail.com>
2023-09-26 13:59:38 +0300

    pushing questions no 15.2

---
+++ javascript/debbuging/smallest.js
@@ -0,0 +1,16 @@
+function findSmallest(a, b, c){
+    if (a > b > c){
+    return c;
+    } else if (a > c > b) {
+    return a;
+    } else {
+    return b;
+    }
+    }
+    findSmallest(52,66, 2)
+
+// The problemtic line is number 10.  it's not wrriten the same as the upper line
+// I have saw it with my eye's.
+//  the name of the function and the the name that we are calling that function are not the same.
+//
+       
