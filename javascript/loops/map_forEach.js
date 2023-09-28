acf8472b23a00e683625636232019ab37f099ee2
pelech-tech <pelech.tech@gmail.com>
2023-09-28 12:38:27 +0300

    uploading ex. 11.1

---
+++ javascript/loops/map_forEach.js
@@ -0,0 +1,15 @@
+let movies = [
+    { id: 1, name: 'The Shawshank Redemption' },
+    { id: 2, name: 'The Godfather' },
+    { id: 3, name: 'The Dark Knight' },
+    { id: 4, name: 'Pulp Fiction' },
+    { id: 5, name: 'Schindler\'s List' },
+    { id: 6, name: 'Forrest Gump' },
+    { id: 7, name: 'The Matrix' },
+    { id: 8, name: 'Inception' },
+]
+  
+movies.forEach(function (movies) {
+    console.log(movies.id, movies.name);     
+});
+
