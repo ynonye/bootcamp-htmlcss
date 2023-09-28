f03e2d59f42c08585cdff6339d12a9d452356736
pelech-tech <pelech.tech@gmail.com>
2023-09-28 10:48:35 +0300

    pushing the last ex

---
+++ coding with others/targilim.js
@@ -0,0 +1,29 @@
+//const nums = [223,4567, 123,99];
+
+//function myFilter(originalArray, callBack) {
+//    const newArray = [];
+//    for (let i = 0; i < originalArray.length; i ++) {
+//        const element = originalArray[i];
+//        const boolResult = callBack(element);
+//        if (boolResult) {
+//            newArray.push(element);
+//        }
+//    }
+//    return newArray;
+//}
+
+//const bigger = myFilter(nums, (num) => {
+//    if (num > 100) {
+//        return true;
+//    }
+//})
+
+//console.log('bigger:', bigger);
+
+const nums = [123,456,789,234,22];
+
+function MyMap(originalArray, callBack) {
+    const newArray = [];
+    
+
+}
