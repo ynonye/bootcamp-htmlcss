b09d814718f120102551d12ef73396f4191957ff
pelech-tech <pelech.tech@gmail.com>
2023-10-01 15:58:51 +0300

    pushing the last changes

--- javascript/1.10.23/exsersises.js
+++ javascript/1.10.23/exsersises.js
@@ -19,15 +19,40 @@
 
 // Block 3:  It will return an undefined and a number.  
 
-function funcB() {
-    let a = 0;
-    let b = 0;
-    a++;
-    return a;
-    }
-function funcA() {
-    console.log(typeof a);
-    console.log(typeof b);
-}    
+//function funcB() {
+//    let a = 0;
+//    let b = 0;
+//    a++;
+//    return a;
+//    }
+//function funcA() {
+//    console.log(typeof a);
+//    console.log(typeof b);
+//}    
 
-// Block 4:  
+// Block 4:  It dosn't print anything.
+//function funcC() {
+//    console.log("1");
+//    console.log("2");
+//    }
+//funcC();     
+
+//Block 5: e is not defined. 
+//function funcD1() {
+//    d = 1;
+//    }
+//    funcD1();
+//    console.log(d);
+//    function funcD2() {
+//    var e = 1;
+//    }
+//    funcD2();
+//    console.log(e);    
+
+// Block 6  - It will print 1. / It didn't printed anything.  
+//function funcE() {
+//    console.log("Value of f in local scope: ", f);
+//    }
+//    console.log("Value of f in global scope: ", f);
+//    var f = 1;
+//    funcE();    
