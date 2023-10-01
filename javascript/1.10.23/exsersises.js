ca3b248e1de270fb28677b27ac63e169ddf3d435
pelech-tech <pelech.tech@gmail.com>
2023-10-01 10:46:19 +0300

    pushing 1/10/2023 ex

---
+++ javascript/1.10.23/exsersises.js
@@ -0,0 +1,33 @@
+// Block 1 - it will print 1, undefind, 2.
+//  It has returned undefined, 2.
+
+//block 2 - The outcome of the code is nothing. 
+
+//var fullName = 'John Doe';
+//var obj = {
+//    fullName: 'Colin Ihrig',
+//    prop: {
+//        fullName: 'Aurelio De Rosa',
+//        getFullName: function () {
+//        return this.fullName;
+//        }
+//    }
+//};
+//console.log(obj.prop.getFullName());
+//var test = obj.prop.getFullName;
+//console.log(test());
+
+// Block 3:  It will return an undefined and a number.  
+
+function funcB() {
+    let a = 0;
+    let b = 0;
+    a++;
+    return a;
+    }
+function funcA() {
+    console.log(typeof a);
+    console.log(typeof b);
+}    
+
+// Block 4:  
