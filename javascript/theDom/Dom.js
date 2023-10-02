7ac7767877a62160ac69780d466dfe10df69f3ff
pelech-tech <pelech.tech@gmail.com>
2023-10-02 11:15:45 +0300

    finished the first ex

---
+++ javascript/theDom/Dom.js
@@ -0,0 +1,30 @@
+<!DOCTYPE html>   
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible"
+content="IE=edge" />
+<meta name="viewport"
+content="width=device-width,
+initial-scale=1.0" />
+<title>Document</title>
+</head>
+<body>
+<h1>Header</h1>
+<p>Paragraph</p>
+<a href="#">link</a>
+<img src="" alt="" />
+<div class="x">Div with a class</div>
+<div id="y">Div with an id</div>
+<input type="text" />
+<input type="text" />
+<input type="text" />
+</body>
+<script>
+    querySelectorAll(``);
+    querySelectorAll(``);
+    const =  inputs;
+      inputs.forEach(function (querySelectorAll, getElementsByTagName ))
+        consol.log("querySelectorAll, getElementsByTagName");
+</script>
+</html>
