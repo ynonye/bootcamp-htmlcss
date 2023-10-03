5ac5b75094727cba0453ad205435e820d15ae8a1
pelech-tech <pelech.tech@gmail.com>
2023-10-03 18:43:00 +0300

    pushing ex. 21.3

--- javascript/theDom/21.3_text_area.js
+++ javascript/theDom/21.3_text_area.js
@@ -1,4 +1,16 @@
-//const input = document.createElement("input");
+const form = document.querySelector('form');
+const textarea = document.querySelector('textarea');
+const input = document.querySelector('input');
 
-input.setAttribute("type", "text");
-document.body.appendChild(input);
+let p = document.createElement('p');
+textarea.addEventListener('input',e =>{
+    if(e.target.value.length < 100){
+        p.innerText = 'you need to write at least 100 letters';
+    }else{
+        p.innerText='';
+    }
+});
+form.addEventListener('submit', e =>{
+    e.preventDefault();
+    form.append(p);
+});
