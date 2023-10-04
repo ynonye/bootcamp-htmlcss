c585b0e5091de68df7734a2db49488b72155c35e
pelech-tech <pelech.tech@gmail.com>
2023-10-04 11:15:52 +0300

    The ex 21.1

---
+++ javascript/theDom/22.1_forms.js
@@ -0,0 +1,10 @@
+const username = document.querySelector('#username');
+const age = document.querySelector('#age');
+const email = document.querySelector('#email');
+
+
+form.addEventListener('submit', function(e) {
+    alert('Summited the form');
+    console
+    e.preventDefault();
+});
