34b1f246a1a284219ab3fe9f38dd451388fa733f
pelech-tech <pelech.tech@gmail.com>
2023-10-05 16:45:07 +0300

    starting the ex

---
+++ mission 5.10.23 - DiceGame/script.js
@@ -0,0 +1,16 @@
+/* reffering to player 1 */
+
+const score01 = document.getElementById('score_1');
+
+const score02 = document.getElementById('score_2');
+
+const dice = document.getElementById('.dice');
+
+score01.textContent = 0;
+score02.textContent = 0;
+
+
+
+
+
+
